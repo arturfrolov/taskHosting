dbee9f503294e107b5b2d7f451342d7cb2f13651
Artur <djajikamail@gmail.com>
2023-05-24 11:35:19 +0300

    fixed a bug in the 'href' script

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
   $('ul.plan__tabs').on('click', 'li:not(.plan__tab_active)', function() {
     $(this)
       .addClass('plan__tab_active').siblings().removeClass('plan__tab_active')
-      .closest('div.container').find('div.plan__content').removeClass('plan__content_active').eq($(this).index()).addClass('plan__content_active');
+      .closest('div.container').find('div.plan__content')
+      .removeClass('plan__content_active').eq($(this).index()).addClass('plan__content_active');
   });
 
   $('.modal__close').on('click', function() {
@@ -16,7 +17,7 @@ $(document).ready(function(){
     });
   });
 
-  $("a[href='#']").click(function() {
+  $("a[href^='#']").click(function() {
     const _href = $(this).attr("href");
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
